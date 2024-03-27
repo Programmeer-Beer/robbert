723c811925609eaf3f8127966b92d87ccb3fabef
Jort <accounts@jajacobs.com>
2024-03-27 17:37:28 +0100

    Update config variables

--- config.example.py
+++ config.example.py
@@ -14,7 +14,7 @@ ROBBERT_LOGO = """
 
 # Robbert
 STALL_PERCENTAGE = 40  # [%]
-MIN_DISTANCE = 25  # [cm]
+MIN_DISTANCE = 15  # [cm]
 AVOID_COLLISSION_SPEED = 20  # [%]
 AVOID_COLLISSION_BACKWARD_DURATION = 0.5  # [s]
 AVOID_COLLISSION_TURN_DURATION = 0.1  # [s]
@@ -22,6 +22,7 @@ DROPPED_TRASH_SPEED = 20  # [s]
 DROPPED_TRASH_DURATION = 1  # [s]
 NON_GRABBED_SERVO_ANGLE= 100 #[]
 GRABBED_SERVO_ANGLE = 50  #[]
+
 
 TURN_LEFT = {
     'direction': 'left',
@@ -39,16 +40,16 @@ RIGHT_MOTOR_DEVIATION = 1 # Multiplycation factor
 # Object detection
 MODEL_NAME = "aluminium_papier_model"
 SHOW_FRAME = False
-resW = 480  # [px]
-resH = 640  # [px]
+resW = 640  # [px]
+resH = 480  # [px]
 min_conf_threshold = 0.80  # [%]
 
 # Calculate
 DEVIATION = 10  # [%]
-DISERED_OBJECT_LOCATION = 10  # [%]
-CAUTION_LEVEL_X = 0.0005  # CAUTION_LEVEL = duration[s] / deviation[%]
-CAUTION_LEVEL_Y = 0.0005  # CAUTION_LEVEL = duration[s] / deviation[%]
+DISERED_OBJECT_LOCATION = 15  # [%]
+CAUTION_LEVEL_X = 0.001  # CAUTION_LEVEL = duration[s] / deviation[%]
+CAUTION_LEVEL_Y = 0.001  # CAUTION_LEVEL = duration[s] / deviation[%]
 STANDARD_SPEED = 90  # [%]
 STANDARD_DURATION_SONAR_FORWARD = 0.1 # [s]
 STANDARD_DURATION_SONAR_TURNING = 0.1 # [s]
-DROP_DISTANCE = 0.2  # [m]
+DROP_DISTANCE = 25  # [cm]
